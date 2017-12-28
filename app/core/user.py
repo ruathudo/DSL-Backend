5c0fba45dda6fce6f5261d3a937537c1b5f346b0
Quan Duong <ruathudo@gmail.com>
2017-12-28 13:06:56 +0200

    implement utils functions

--- app/core/user.py
+++ app/core/user.py
@@ -1,55 +1,52 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.models import User as UserModel
+from app.models import User
 from app.serializers import UserSchema
 from app import db
 from config import error_codes as ec
 from app.utils import format_schema_errors
 
 
-class User(object):
+def register(data):
+    # load data to validate
+    user_schema = UserSchema()
+    new_user, errs = user_schema.load(data)
 
-    @staticmethod
-    def register(data):
-        # load data to validate
-        user_schema = UserSchema()
-        new_user, errs = user_schema.load(data)
+    if errs:
+        return None, format_schema_errors(errs)
 
-        if errs:
-            return None, format_schema_errors(errs)
+    # check duplicated user
+    user = User.query.filter_by(username=data["username"]).one_or_none()
+    if user:
+        return None, ec[409]
 
-        # check duplicated user
-        user = UserModel.query.filter_by(username=data["username"]).one_or_none()
-        if user:
-            return None, ec[409]
+    # generate hash password
+    new_user.password = generate_password_hash(data['password'])
 
-        # generate hash password
-        new_user.password = generate_password_hash(data['password'])
+    # add to db
+    db.session.add(new_user)
+    db.session.commit()
+    db.session.refresh(new_user)
 
-        # add to db
-        db.session.add(new_user)
-        db.session.commit()
-        db.session.refresh(new_user)
+    # serialize data
+    user_data, errs = user_schema.dump(new_user)
 
-        # serialize data
-        user_data, errs = user_schema.dump(new_user)
+    return user_data, errs
 
-        return user_data, errs
 
-    @staticmethod
-    def login(username, password):
-        # check user
-        user = UserModel.query.filter_by(username=username).one_or_none()
+def login(username, password):
+    # check user
+    user = User.query.filter_by(username=username).one_or_none()
 
-        if not user:
-            return None, ec[404]
-        # check password
-        check_password = check_password_hash(user.password, password)
+    if not user:
+        return None, ec[404]
+    # check password
+    check_password = check_password_hash(user.password, password)
 
-        if not check_password:
-            return None, ec[401]
-        # serialize data
-        user_schema = UserSchema()
-        user_data, errs = user_schema.dump(user)
+    if not check_password:
+        return None, ec[401]
+    # serialize data
+    user_schema = UserSchema()
+    user_data, errs = user_schema.dump(user)
 
-        return user_data, None
+    return user_data, None
